d92f61deb7a79dc6859dca17390142dcd976d0ec
Override In Gituser File (~/.gituser) <see@gituser.example.file>
2022-02-28 01:02:43 -0800

    circles of latitude and longitude

--- webgl-globe.js
+++ webgl-globe.js
@@ -25,12 +25,22 @@ class WebglGlobeElement extends HTMLElement {
     this.renderer.setSize(window.innerWidth, window.innerHeight)
     this.appendChild(this.renderer.domElement)
 
-    this.sphere = this.getSphere()
+    this.sphere = this.getSphere(0.998)
+    this.circlesOfLatitude = []
+    for (let latitude = -75; latitude <= 75; latitude += 15) {
+      this.circlesOfLatitude.push(this.getCircleOfLatitude(latitude))
+    }
+    this.circlesOfLongitude = []
+    for (let longitude = 0; longitude < 180; longitude += 15) {
+      this.circlesOfLongitude.push(this.getCircleOfLongitude(longitude))
+    }
     this.glow = new THREE.AmbientLight(0xffffff, 1)
     this.sun = new THREE.DirectionalLight(0xffffff, 1)
     this.sun.position.set(5, 3, 5)
 
     this.scene.add(this.sphere)
+    this.circlesOfLatitude.forEach(curve => this.scene.add(curve))
+    this.circlesOfLongitude.forEach(curve => this.scene.add(curve))
     this.scene.add(this.glow)
     this.scene.add(this.sun)
 
@@ -42,23 +52,66 @@ class WebglGlobeElement extends HTMLElement {
     this.camera = null
     this.renderer = null
     this.sphere = null
+    this.circlesOfLatitude = null
+    this.circlesOfLongitude = null
     this.glow = null
     this.sun = null
     this.innerHTML = ''
   }
 
-  getSphere() {
-    const geometry = new THREE.SphereGeometry(1, 48, 24)  // unit sphere
-    const material = new THREE.MeshLambertMaterial({color: 'gray'})
+  getSphere(radius) {
+    const geometry = new THREE.SphereGeometry(radius, 48, 24)
+    const material = new THREE.MeshLambertMaterial({color: 0xFFFFFF})
     return new THREE.Mesh(geometry, material)
   }
 
-  render() {
-    // FUTURE: animate globe
-    // requestAnimationFrame(this.render.bind(this))
+  getCircleOfLatitude(latitude) {
+    // angle in radians
+    const angle = latitude * Math.PI / 180
 
-    // FUTURE: rotate around poles
-    // this.sphere.rotation.y += 0.01
+    // calculate radius from angle
+    const radius = Math.cos(angle)
+
+    // circle in same plane as prime meridian
+    const curve = new THREE.EllipseCurve(0, 0, radius, radius)
+    const points = curve.getSpacedPoints(48)
+    const geometry = new THREE.BufferGeometry().setFromPoints(points)
+    const material = new THREE.LineBasicMaterial({color: 0xEEEEEE, linewidth: 3})
+    const line = new THREE.Line(geometry, material)
+
+    // rotate circle to be in same plane as equator
+    const quaternion = new THREE.Quaternion()
+    line.rotation.x += Math.PI / 2
+
+    // translate circle towards north or south pole
+    line.position.y += Math.sin(angle)
+
+    return line
+  }
+
+  getCircleOfLongitude(longitude) {
+    // angle in radians
+    const angle = longitude * Math.PI / 180
+
+    // great circle of 0 degrees (prime meridian) and 180 degrees
+    const curve = new THREE.EllipseCurve()
+    const points = curve.getSpacedPoints(48)
+    const geometry = new THREE.BufferGeometry().setFromPoints(points)
+    const material = new THREE.LineBasicMaterial({color: 0xEEEEEE, linewidth: 3})
+    const line = new THREE.Line(geometry, material)
+
+    // rotate great circle about poles
+    line.rotation.y += angle
+
+    return line
+  }
+
+  render() {
+    // animate globe
+    requestAnimationFrame(this.render.bind(this))
+
+    // rotate around poles
+    this.circlesOfLongitude.forEach(curve => curve.rotation.y += 0.01)
 
     this.renderer.render(this.scene, this.camera)
   }
