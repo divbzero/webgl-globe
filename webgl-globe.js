1094c09f1d3613fff39e05496f8e0baced10f77a
Override In Gituser File (~/.gituser) <see@gituser.example.file>
2022-02-28 14:36:54 -0800

    lines from GeoJSON

--- webgl-globe.js
+++ webgl-globe.js
@@ -15,7 +15,7 @@ class WebglGlobeElement extends HTMLElement {
     }
   }
 
-  connectedCallback() {
+  async connectedCallback() {
     this.scene = new THREE.Scene()
 
     this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000)
@@ -34,6 +34,7 @@ class WebglGlobeElement extends HTMLElement {
     for (let longitude = 0; longitude < 180; longitude += 15) {
       this.circlesOfLongitude.push(this.getCircleOfLongitude(longitude))
     }
+    this.geojson = await this.getGeoJSON('countries.json')
     this.glow = new THREE.AmbientLight(0xffffff, 1)
     this.sun = new THREE.DirectionalLight(0xffffff, 1)
     this.sun.position.set(5, 3, 5)
@@ -41,6 +42,7 @@ class WebglGlobeElement extends HTMLElement {
     this.scene.add(this.sphere)
     this.circlesOfLatitude.forEach(curve => this.scene.add(curve))
     this.circlesOfLongitude.forEach(curve => this.scene.add(curve))
+    this.geojson.forEach(line => this.scene.add(line))
     this.scene.add(this.glow)
     this.scene.add(this.sun)
 
@@ -54,6 +56,7 @@ class WebglGlobeElement extends HTMLElement {
     this.sphere = null
     this.circlesOfLatitude = null
     this.circlesOfLongitude = null
+    this.geojson = null
     this.glow = null
     this.sun = null
     this.innerHTML = ''
@@ -106,12 +109,79 @@ class WebglGlobeElement extends HTMLElement {
     return line
   }
 
+  async getGeoJSON(url) {
+    const lines = []
+
+    const response = await fetch(url)
+    const geojson = await response.json()
+
+    switch (geojson.type) {
+      case 'FeatureCollection':
+        geojson.features.forEach(feature => lines.push.apply(lines, this.getGeoJSONFeature(feature)))
+        break
+    }
+
+    return lines
+  }
+
+  /**
+   * @typedef {[Number, Number]} Coordinates
+   * @param {Object} feature
+   * @param {Object} feature.geometry
+   * @param {String} feature.geometry.type
+   * @param {Coordinates[]|Coordinates[][]} feature.geometry.coordinates
+   * @returns {THREE.Object3D[]}
+   */
+  getGeoJSONFeature(feature) {
+    const objects = []
+
+    let points, geometry, material
+    switch (feature.geometry.type) {
+      case 'LineString':
+        points = feature.geometry.coordinates.map(this.getUnitSphereCoordinates)
+        geometry = new THREE.BufferGeometry().setFromPoints(points)
+        material = new THREE.LineBasicMaterial({color: 0x888888, linewidth: 3})
+        objects.push(new THREE.Line(geometry, material))
+        break
+      case 'MultiLineString':
+        feature.geometry.coordinates.forEach(coordinates => {
+          const points = coordinates.map(this.getUnitSphereCoordinates)
+          const geometry = new THREE.BufferGeometry().setFromPoints(points)
+          const material = new THREE.LineBasicMaterial({color: 0x888888, linewidth: 3})
+          objects.push(new THREE.Line(geometry, material))
+        })
+        break
+    }
+
+    return objects
+  }
+
+  /**
+   * @param {Number} longitude
+   * @param {Number} latitude
+   * @returns {THREE.Vector3}
+   */
+  getUnitSphereCoordinates([longitude, latitude]) {
+    longitude *= Math.PI / 180
+    latitude *= Math.PI / 180
+
+    // positive x-axis intersects prime meridian and equator (0°E 0°N)
+    const x = Math.cos(latitude) * Math.cos(longitude)
+    // positive y-axis intersects north pole (0°E 90°N)
+    const y = Math.sin(latitude)
+    // positive z-axis intersects 90°W and equator (90°W 0°N)
+    const z = -Math.cos(latitude) * Math.sin(longitude)
+
+    return new THREE.Vector3(x, y, z)
+  }
+
   render() {
     // animate globe
     requestAnimationFrame(this.render.bind(this))
 
     // rotate around poles
     this.circlesOfLongitude.forEach(curve => curve.rotation.y += 0.01)
+    this.geojson.forEach(line => line.rotation.y += 0.01)
 
     this.renderer.render(this.scene, this.camera)
   }
